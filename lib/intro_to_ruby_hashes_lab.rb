3595b0871de357ea4b24759cdfedad45b316ed9a
Devin Davis <devinrdavis555@gmail.com>
2020-08-03 17:42:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,15 @@ def new_hash
 end
 
 def my_hash
-{ pets: "dog" }
+ { pets: "dog" }
 end
 
 def pioneer
   { name: "Grace Hopper" }
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  another_hash = { :id => number }
-  # return a hash with a key :id assigned to the provided number
+ { :id => number }
+
 end
